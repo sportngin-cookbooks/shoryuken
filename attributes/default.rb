f64a70f5b81c7ebd9166d57c648d455654d83b96
Andy Fleener <andrew.fleener@sportngin.com>
2015-09-04 08:46:13 -0500

    Support running multiple processes

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,7 +1,7 @@
 default[:shoryuken][:app_dir] = nil
 default[:shoryuken][:config] = "config/shoryuken.yml"
 default[:shoryuken][:logfile] = "log/shoryuken.log"
-default[:shoryuken][:pidfile] = "tmp/pids/shoryuken.pid"
+default[:shoryuken][:pidsdir] = "tmp/pids"
 default[:shoryuken][:bundle_exec] = true
 default[:shoryuken][:rails] = true
 default[:shoryuken][:concurrency] = nil
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,20 +1,25 @@
 include_recipe "runit"
-command = "chpst"
-command += " -u #{node[:shoryuken][:user]} " if node[:shoryuken][:user]
-command += " -e /etc/sv/shoryuken/env" if node[:shoryuken][:environment_variables]
-command += " bundle exec" if node[:shoryuken][:bundle_exec]
-command += " shoryuken"
-command += " --verbose" if node[:shoryuken][:verbose]
-command += " --rails" if node[:shoryuken][:rails]
-command += " --pidfile #{node[:shoryuken][:pidfile]}" if node[:shoryuken][:pidfile]
-command += " --logfile #{node[:shoryuken][:logfile]}" if node[:shoryuken][:logfile]
-command += " --config #{node[:shoryuken][:config]}" if node[:shoryuken][:config]
-command += " --concurrency #{node[:shoryuken][:concurrency]}" if node[:shoryuken][:concurrency]
-command += " --require #{node[:shoryuken][:require]}" if node[:shoryuken][:require]
 
-runit_service "shoryuken" do
-  action node[:shoryuken][:service_action]
-  options(:command => command, :app_dir => node[:shoryuken][:app_dir])
-  env node[:shoryuken][:environment_variables]
-  sv_timeout node[:shoryuken][:service_timeout]
+(0...node[:shoryuken][:number_of_processes]).each do |service_id|
+  command = "chpst"
+  command += " -u #{node[:shoryuken][:user]} " if node[:shoryuken][:user]
+  command += " -e /etc/sv/shoryuken-#{service_id}/env" if node[:shoryuken][:environment_variables]
+  command += " bundle exec" if node[:shoryuken][:bundle_exec]
+  command += " shoryuken"
+  command += " --verbose" if node[:shoryuken][:verbose]
+  command += " --rails" if node[:shoryuken][:rails]
+  command += " --pidfile #{node[:shoryuken][:pidsdir]}/shoryuken-#{service_id}.pid" if node[:shoryuken][:pidsdir]
+  command += " --logfile #{node[:shoryuken][:logfile]}" if node[:shoryuken][:logfile]
+  command += " --config #{node[:shoryuken][:config]}" if node[:shoryuken][:config]
+  command += " --concurrency #{node[:shoryuken][:concurrency]}" if node[:shoryuken][:concurrency]
+  command += " --require #{node[:shoryuken][:require]}" if node[:shoryuken][:require]
+
+  runit_service "shoryuken-#{service_id}" do
+    action node[:shoryuken][:service_action]
+    options(:command => command, :app_dir => node[:shoryuken][:app_dir])
+    env node[:shoryuken][:environment_variables]
+    sv_timeout node[:shoryuken][:service_timeout]
+    run_template_name "shoryuken"
+    log_template_name "shoryuken"
+  end
 end
